578ea8531b4de14f33fd02eaa9617f757d51729b
milovanovmaksim <milovanov160386@gmail.com>
2024-09-27 23:06:09 +0300

    add trait UserRepository

--- src/app/core/user/repository.rs
+++ src/app/core/user/repository.rs
@@ -1,5 +1,6 @@
 use super::entity::User;
+use crate::app::core::error::AppError;
 
-pub trait UserRepository {
-    fn signup(&self, email: &str, username: &str, naive_password: &str) -> Result<User, AppError>
+pub trait UserRepository: Send + Sync + 'static {
+    fn signup(&self, email: &str, username: &str, naive_password: &str) -> Result<User, AppError>;
 }
