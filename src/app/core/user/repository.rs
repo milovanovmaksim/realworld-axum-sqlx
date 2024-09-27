4f8aae392635c317d836bbf262cd403ea4c128cb
milovanovmaksim <milovanov160386@gmail.com>
2024-09-28 01:10:26 +0300

    added async for signup func

--- src/app/core/user/repository.rs
+++ src/app/core/user/repository.rs
@@ -1,6 +1,11 @@
 use super::entity::User;
 use crate::app::core::error::AppError;
 
-pub trait UserRepository: Send + Sync + 'static {
-    fn signup(&self, email: &str, username: &str, naive_password: &str) -> Result<User, AppError>;
+pub trait UsersRepository: Send + Sync + 'static {
+    async fn signup(
+        &self,
+        email: &str,
+        username: &str,
+        naive_password: &str,
+    ) -> Result<User, AppError>;
 }
