a0b038aef3f66b3ea829383193d2fca0b3dad4ce
milovanovmaksim <milovanov160386@gmail.com>
2024-09-28 01:09:28 +0300

    add default value for some users table fields

--- migrations/20240927203548_create_users.up.sql
+++ migrations/20240927203548_create_users.up.sql
@@ -3,12 +3,12 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table
     if not exists users (
-        id UUID primary key default uuid_generate_v4 (),
+        id UUID primary key default uuid_generate_v4(),
         username varchar not null,
         email varchar not null,
         password varchar not null,
-        bio varchar not null,
-        image varchar not null,
+        bio varchar default '',
+        image varchar default '',
         created_at timestamptz not null default current_timestamp,
         updated_at timestamptz not null default current_timestamp
     );
