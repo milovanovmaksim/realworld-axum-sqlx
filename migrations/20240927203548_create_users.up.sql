3a07dc2e568b5c76d4203adb147786c4778eb5e6
milovanovmaksim <milovanov160386@gmail.com>
2024-09-27 23:56:09 +0300

    added migration for users table

---
+++ migrations/20240927203548_create_users.up.sql
@@ -0,0 +1,16 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table
+    if not exists users (
+        id UUID primary key default uuid_generate_v4 (),
+        username varchar not null,
+        email varchar not null,
+        password varchar not null,
+        bio varchar not null,
+        image varchar not null,
+        created_at timestamptz not null default current_timestamp,
+        updated_at timestamptz not null default current_timestamp
+    );
+
+create index if not exists users_email_idx on users (email);
