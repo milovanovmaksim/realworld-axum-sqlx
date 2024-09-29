7a1f3d4cbc1070620d5a14229c49747100ff0ac5
milovanovmaksim <milovanov160386@gmail.com>
2024-09-29 19:34:45 +0300

    delete method

--- src/app/infrastructure/pgsql/settings.rs
+++ src/app/infrastructure/pgsql/settings.rs
@@ -1,9 +1,3 @@
-use dotenv::dotenv;
-use sqlx::postgres::{PgConnectOptions, PgSslMode};
-use std::env;
-
-use crate::app::constants::env_key;
-
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
@@ -11,6 +5,7 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
     pub require_ssl: bool,
+    pub max_connections: u32,
 }
 
 impl DatabaseSettings {
@@ -21,6 +16,7 @@ impl DatabaseSettings {
         host: String,
         database_name: String,
         require_ssl: bool,
+        max_connections: u32,
     ) -> Self {
         Self {
             username,
@@ -29,26 +25,7 @@ impl DatabaseSettings {
             host,
             database_name,
             require_ssl,
-        }
-    }
-
-    pub fn from_env(require_ssl: bool) -> Self {
-        dotenv().ok();
-        let password = env::var(env_key::DATABASE_PASSWORD).expect("DATABASE_PASSWORD must be set");
-        let username = env::var(env_key::DATABASE_USERNAME).expect("DATABASE_USERNAME must be set");
-        let port = env::var(env_key::DATABASE_PORT)
-            .expect("DATABASE_PORT must be set")
-            .parse::<u16>()
-            .expect("failed to parse DATABASE_PORT");
-        let host = env::var(env_key::DATABASE_HOST).expect("DATABASE_HOST must be set");
-        let database_name = env::var(env_key::DATABASE_NAME).expect("DATABASE_NAME must be set");
-        Self {
-            username,
-            password,
-            port,
-            host,
-            database_name,
-            require_ssl,
+            max_connections,
         }
     }
 }
