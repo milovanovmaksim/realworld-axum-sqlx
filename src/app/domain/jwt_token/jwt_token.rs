d1a85e7bd35692f02d9962c8fc71746747be592a
milovanovmaksim <milovanov160386@gmail.com>
2024-09-29 16:40:59 +0300

    delete async_traie for JwtAuthToken

--- src/app/domain/jwt_token/jwt_token.rs
+++ src/app/domain/jwt_token/jwt_token.rs
@@ -1,9 +1,8 @@
-use async_trait::async_trait;
 use uuid::Uuid;
 
 use crate::app::domain::error::AppError;
 
-#[async_trait]
+
 pub trait JwtAuthToken: Send + Sync + 'static {
     fn generate_token(&self, user_id: Uuid, now: i64, offset: i64) -> Result<String, AppError>;
 }
