48c095d6909fd2abde070ababe408ea876be0ebd
milovanovmaksim <milovanov160386@gmail.com>
2024-12-23 21:43:28 +0300

    renamed a field

--- src/app/api/user/router.rs
+++ src/app/api/user/router.rs
@@ -1,5 +1,10 @@
-use crate::app::infrastructure::{di::DiContainer, user::usecase::UserUseCaseImpl};
+use crate::app::{
+    api::extractors::validation_extractor::ValidationExtractor,
+    infrastructure::{di::DiContainer, user::usecase::UserUseCaseImpl},
+};
 use axum::{routing::post, Extension, Router};
+
+use super::requests::Signup;
 
 pub fn user_router(di: DiContainer) -> Router {
     Router::new().route("/users", post(signup))
@@ -7,6 +12,6 @@ pub fn user_router(di: DiContainer) -> Router {
 
 pub async fn signup(
     ValidationExtractor(request): ValidationExtractor<Signup>,
-    Extension(users_service): Extension<UserUseCaseImpl>,
+    Extension(user_usecase): Extension<UserUseCaseImpl>,
 ) {
 }
