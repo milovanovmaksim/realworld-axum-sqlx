b611776799f6828e505b66bb8ea726aaf09bf0c3
milovanovmaksim <milovanov160386@gmail.com>
2024-12-23 16:41:05 +0300

    added requests

---
+++ src/app/api/user/requests.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Deserialize, Serialize, Debug, Validate)]
+pub struct Signup {
+    #[validate]
+    pub user: SignupUser,
+}
+
+#[derive(Deserialize, Serialize, Debug, Validate)]
+pub struct SignupUser {
+    #[validate(required, length(min = 1))]
+    pub username: Option<String>,
+
+    #[validate(required, length(min = 1), email(message = "email is invalid"))]
+    pub email: Option<String>,
+
+    #[validate(required, length(min = 8))]
+    pub password: Option<String>,
+}
