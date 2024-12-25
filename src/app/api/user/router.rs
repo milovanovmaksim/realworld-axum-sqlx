3f81e738c51b60b3c8bd998dc19ccf58bb98cae9
milovanovmaksim <milovanov160386@gmail.com>
2024-12-26 00:08:59 +0300

    added signup controller

--- src/app/api/user/router.rs
+++ src/app/api/user/router.rs
@@ -1,17 +1,28 @@
+use std::sync::Arc;
+
 use crate::app::{
-    api::extractors::validation_extractor::ValidationExtractor,
+    api::{extractors::validation_extractor::ValidationExtractor, response::ApiResponse},
+    domain::user::usecase::{
+        requests::SignupRequest as UserUsecaseSignupRequest, usecase::UserUseCase,
+    },
     infrastructure::{di::DiContainer, user::usecase::UserUseCaseImpl},
 };
-use axum::{routing::post, Extension, Router};
+use axum::{routing::post, Extension, Json, Router};
 
-use super::requests::Signup;
+use super::{requests::SignupRequest, responses::SignupResponse};
 
 pub fn user_router(di: DiContainer) -> Router {
-    Router::new().route("/users", post(signup))
+    Router::new()
+        .route("/users", post(signup))
+        .layer(Extension(di.user_usecase))
+        .layer(Extension(di.jwt_auth_token))
 }
 
 pub async fn signup(
-    ValidationExtractor(request): ValidationExtractor<Signup>,
-    Extension(user_usecase): Extension<UserUseCaseImpl>,
-) {
+    ValidationExtractor(request): ValidationExtractor<SignupRequest>,
+    Extension(user_usecase): Extension<Arc<UserUseCaseImpl>>,
+) -> ApiResponse<Json<SignupResponse>> {
+    let request = UserUsecaseSignupRequest::from(request);
+    let user = user_usecase.signup(request).await?;
+    Ok(Json(SignupResponse::from(user)))
 }
