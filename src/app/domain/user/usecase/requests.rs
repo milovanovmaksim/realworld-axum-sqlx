b1a0c58bf592b7f3bd3e7848eebf729438b2cb85
milovanovmaksim <milovanov160386@gmail.com>
2024-12-26 00:09:45 +0300

    implemented From traite

--- src/app/domain/user/usecase/requests.rs
+++ src/app/domain/user/usecase/requests.rs
@@ -1,3 +1,5 @@
+use crate::app::api::user::requests as user_api_requests;
+
 pub struct SignupRequest {
     pub username: String,
     pub email: String,
@@ -7,4 +9,14 @@ pub struct SignupRequest {
 pub struct SigninRequest {
     pub email: String,
     pub naive_password: String,
+}
+
+impl From<user_api_requests::SignupRequest> for SignupRequest {
+    fn from(value: user_api_requests::SignupRequest) -> Self {
+        SignupRequest {
+            username: value.user.username.unwrap(),
+            email: value.user.email.unwrap(),
+            naive_password: value.user.password.unwrap(),
+        }
+    }
 }
