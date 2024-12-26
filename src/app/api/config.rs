074776ad4337b3b5150f84b7c3403b0f8418ea11
milovanovmaksim <milovanov160386@gmail.com>
2024-12-26 15:45:55 +0300

    changed returning type

--- src/app/api/config.rs
+++ src/app/api/config.rs
@@ -5,23 +5,25 @@ use serde_yaml::Mapping;
 
 #[derive(Deserialize)]
 pub struct ApiConfig {
-    host: String,
-    port: u32,
-    frontend_origin: String,
+    pub host: String,
+    pub port: u16,
+    pub frontend_origin: String,
 }
 
 impl ApiConfig {
-    pub fn from_yaml<T: AsRef<Path>>(path: T) -> Self {
-        let file =
-            File::open(path).expect("DatabaseSettings::form_yaml || error: failed to open file");
-        let res: Mapping = serde_yaml::from_reader(file)
-            .expect("DatabaseSettings::form_yaml || error: failed to parse data from file");
+    pub fn from_yaml<T: AsRef<Path>>(path: T) -> Result<Self, String> {
+        let file = File::open(path).map_err(|e| {
+            format!("DatabaseSettings::form_yaml || error: failed to open file {e}")
+        })?;
+        let res: Mapping = serde_yaml::from_reader(file).map_err(|e| {
+            format!("DatabaseSettings::form_yaml || error: failed to parse data from file {e}")
+        })?;
         let value = res
             .get("api")
-            .expect("ApiConfig::form_yaml || error: key 'api' does not exist");
-        let config = ApiConfig::deserialize(value).expect(
-            "DatabaseSettings::form_yaml || error: failed to deserialize database settings",
-        );
-        config
+            .ok_or("ApiConfig::form_yaml || error: key 'api' does not exist")?;
+        let config = ApiConfig::deserialize(value).map_err(|e| {
+            format!("ApiConfig::form_yaml || error: failed to deserialize api settings {e}",)
+        })?;
+        Ok(config)
     }
 }
