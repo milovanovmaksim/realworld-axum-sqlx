cf8d05a49eb201cabedbd5278505c6543b4861e2
milovanovmaksim <milovanov160386@gmail.com>
2024-12-26 15:46:09 +0300

    implemented start

--- src/app/api/server.rs
+++ src/app/api/server.rs
@@ -16,7 +16,17 @@ impl Server {
             api_config,
         }
     }
-    pub fn start(&self) {
+    pub async fn start(&self) -> Result<(), String> {
         let router = Router::new().nest("/api", user_router(self.di_container.clone()));
+
+        let soket_addr = &format!("{}:{}", self.api_config.host, self.api_config.port)
+            .parse()
+            .map_err(|e| format!("Server::start || error: failed to parse soket addres {e}"))?;
+
+        axum::Server::bind(&soket_addr)
+            .serve(router.into_make_service())
+            .await
+            .map_err(|e| format!("Server::start || error: failed to start server {e}"))?;
+        Ok(())
     }
 }
