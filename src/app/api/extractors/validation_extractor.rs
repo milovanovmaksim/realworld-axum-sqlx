6e183567bc10d4f12e9cafda9c7e83449e00ef90
milovanovmaksim <milovanov160386@gmail.com>
2024-12-27 22:22:11 +0300

    implemented FromRequest for ValidationExtractor

--- src/app/api/extractors/validation_extractor.rs
+++ src/app/api/extractors/validation_extractor.rs
@@ -1,9 +1,5 @@
 use async_trait::async_trait;
-use axum::{
-    body::HttpBody,
-    extract::{FromRequest, RequestParts},
-    BoxError, Json,
-};
+use axum::{extract::FromRequest, Json};
 use serde::de::DeserializeOwned;
 use validator::Validate;
 
@@ -12,17 +8,15 @@ use crate::app::domain::error::AppError;
 pub struct ValidationExtractor<T>(pub T);
 
 #[async_trait]
-impl<T, B> FromRequest<B> for ValidationExtractor<T>
+impl<S, T> FromRequest<S> for ValidationExtractor<T>
 where
     T: DeserializeOwned + Validate,
-    B: HttpBody + Send,
-    B::Data: Send,
-    B::Error: Into<BoxError>,
+    S: Send + Sync,
 {
     type Rejection = AppError;
 
-    async fn from_request(request: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let Json(value) = Json::<T>::from_request(request).await?;
+    async fn from_request(req: axum::extract::Request, state: &S) -> Result<Self, Self::Rejection> {
+        let Json(value) = Json::<T>::from_request(req, state).await?;
         value.validate()?;
         Ok(ValidationExtractor(value))
     }
