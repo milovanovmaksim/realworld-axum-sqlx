83d14d094e9c9b61be048793e3941aab0ec2ded8
milovanovmaksim <milovanov160386@gmail.com>
2025-01-13 13:33:32 +0300

    added openapi controller

--- src/app/api/openapi.rs
+++ src/app/api/openapi.rs
@@ -1,15 +1,38 @@
+use axum::{routing::get, Json, Router};
 use utoipa::OpenApi;
 
 use crate::app::api::user::{requests::SignupUserRequest, responses::SignupUserResponse};
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(crate::app::api::user::router::signup),
+    paths(
+        crate::app::api::user::router::signup,
+        openapi
+    ),
     components(
         schemas(SignupUserRequest, SignupUserResponse),
     ),
     tags(
-        (name = "users", description = "Users endpoints")
+        (name = "users", description = "Users endpoints"),
+        (name = "api-docs", description = "Openapi endpoints")
     ),
 )]
-pub struct ApiDocunentation;
+pub struct ApiDocumentation;
+
+
+/// Return JSON version of an OpenAPI schema
+#[utoipa::path(
+    get,
+    path = "/api-docs/openapi.json",
+    responses(
+        (status = 200, description = "JSON file", body = ())
+    )
+)]
+async fn openapi() -> Json<utoipa::openapi::OpenApi> {
+    Json(ApiDocumentation::openapi())
+}
+
+
+pub fn router() -> Router {
+    axum::Router::new().route("/api-docs/openapi.json", get(openapi))
+}
