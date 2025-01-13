d6162c1a41457cab4ab8674359fba9922a7d3584
milovanovmaksim <milovanov160386@gmail.com>
2025-01-13 11:28:26 +0300

    added StatusCode

--- src/app/api/user/router.rs
+++ src/app/api/user/router.rs
@@ -25,14 +25,15 @@ pub fn user_router(di: DiContainer) -> Router {
     request_body = SignupUserRequest,
     description = "Register a new user",
     responses(
-        (status = OK, description = "New user has been created", body = SignupUserResponse),
-        (status = UNPROCESSABLE_ENTITY, description = "Unprocessable entity", body = HashMap<String, HashMap<String, Vec<String>>>,
+        (status = StatusCode::OK, description = "New user has been created", body = SignupUserResponse),
+        (status = StatusCode::UNPROCESSABLE_ENTITY, description = "Unprocessable entity", body = HashMap<String, HashMap<String, Vec<String>>>,
             example = json!({
                 "errors": {
                     "body": ["body is required"]
                 }
             })),
-        (status = INTERNAL_SERVER_ERROR, description = "Internal server error", body = HashMap<String, String>,
+        (status = StatusCode::CONFLICT, description = "Conflict", body = HashMap<String, String>, example=json!({"error": "User with email 'example@gmail.com' allredy exists"})),
+        (status = StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = HashMap<String, String>,
             example = json!({"error": AppError::InternalServerError.to_string()}))
     )
 )]
