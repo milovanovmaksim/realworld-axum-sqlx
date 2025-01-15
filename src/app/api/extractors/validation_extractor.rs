3200d54be4e544b60ec6441c08dd6e76168c7c17
milovanovmaksim <milovanov160386@gmail.com>
2025-01-15 21:18:23 +0300

    deleted async_trait

--- src/app/api/extractors/validation_extractor.rs
+++ src/app/api/extractors/validation_extractor.rs
@@ -1,13 +1,15 @@
-use async_trait::async_trait;
-use axum::{extract::FromRequest, Json};
-use serde::de::DeserializeOwned;
+use axum::{
+    extract::{FromRequest, Request},
+    Json,
+};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use validator::Validate;
 
 use crate::app::domain::error::AppError;
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ValidationExtractor<T>(pub T);
 
-#[async_trait]
 impl<S, T> FromRequest<S> for ValidationExtractor<T>
 where
     T: DeserializeOwned + Validate,
@@ -15,7 +17,7 @@ where
 {
     type Rejection = AppError;
 
-    async fn from_request(req: axum::extract::Request, state: &S) -> Result<Self, Self::Rejection> {
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
         let Json(value) = Json::<T>::from_request(req, state).await?;
         value.validate()?;
         Ok(ValidationExtractor(value))
