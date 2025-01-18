d8890ae3ff92c5b79273e1c85bba7e9e29a6ef85
milovanovmaksim <milovanov160386@gmail.com>
2025-01-19 00:02:35 +0300

    chnaged type in usrs table

--- migrations/20240927203548_create_users.up.sql
+++ migrations/20240927203548_create_users.up.sql
@@ -4,13 +4,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 create table
     if not exists users (
         id UUID primary key default uuid_generate_v4(),
-        username varchar not null,
-        email varchar not null unique,
-        password varchar not null,
-        bio varchar,
-        image varchar,
+        username text not null,
+        email text not null unique,
+        password text not null,
+        bio text,
+        image text,
         created_at timestamptz not null default current_timestamp,
-        updated_at timestamptz not null default current_timestamp
+        updated_at timestamptz default current_timestamp
     );
 
 create index if not exists users_email_idx on users (email);
