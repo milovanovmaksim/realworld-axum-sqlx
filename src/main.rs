77e04e47fe636fb22cde0faaa625fa8bf90b40b0
milovanovmaksim <milovanov160386@gmail.com>
2025-01-18 23:44:56 +0300

    changed main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use crate::app::infrastructure::pgsql::settings::DatabaseSettings;
+use app::{
+    api::{config::ApiConfig, server::Server},
+    infrastructure::di::DiContainer,
+};
 
 mod app;
 mod tests;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
-    let d = DatabaseSettings::from_yaml("./configuration/config.yaml");
-    println!("{:#?}", d);
+    let di_container = DiContainer::new("./configuration/config.yaml")
+        .await
+        .expect("Failed to create di container");
+    let api_config =
+        ApiConfig::from_yaml("./configuration/config.yaml").expect("Failed to create api config");
+    let server = Server::new(di_container, api_config);
+    server.start().await;
 }
