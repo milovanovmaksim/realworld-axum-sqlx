7dea8e596e5077c17c37aeca6925d601c216064a
milovanovmaksim <milovanov160386@gmail.com>
2025-01-19 21:00:05 +0300

    added expect

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ mod tests;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
     let di_container = DiContainer::new("./config/config.yaml")
         .await
         .expect("Failed to create di container");
     let api_config =
-        ApiConfig::from_yaml("./configuration/config.yaml").expect("Failed to create api config");
+        ApiConfig::from_yaml("./config/config.yaml").expect("Failed to create api config");
     let server = Server::new(di_container, api_config);
-    server.start().await;
+    server.start().await.expect("Failed to run server");
 }
