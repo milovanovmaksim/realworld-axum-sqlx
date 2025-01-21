c31b05f42e0322cf7178ce99c28b9c2babeb182f
milovanovmaksim <milovanov160386@gmail.com>
2025-01-21 16:13:17 +0300

    added endpoint

--- src/app/api/user/router.rs
+++ src/app/api/user/router.rs
@@ -1,12 +1,16 @@
 use crate::app::infrastructure::di::DiContainer;
-use axum::{routing::post, Extension, Router};
+use axum::{
+    routing::{get, post},
+    Extension, Router,
+};
 
-use super::endpoints::{login, signup};
+use super::endpoints::{get_current_user, login, signup};
 
 pub fn user_router(di: DiContainer) -> Router {
     Router::new()
         .route("/users", post(signup))
         .route("/users/login", post(login))
+        .route("/user", get(get_current_user))
         .layer(Extension(di.user_usecase))
         .layer(Extension(di.jwt_auth_token))
 }
