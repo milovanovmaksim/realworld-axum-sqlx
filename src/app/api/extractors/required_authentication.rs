6b013e84af3582a313eb1351d71700dbb1deead6
milovanovmaksim <milovanov160386@gmail.com>
2025-01-23 15:03:56 +0300

    added getting JwtAuthTokenImpl from Extension

--- src/app/api/extractors/required_authentication.rs
+++ src/app/api/extractors/required_authentication.rs
@@ -1,5 +1,8 @@
-use axum::{extract::FromRequestParts, http::request::Parts, Extension};
+use std::sync::Arc;
+
+use axum::{extract::FromRequestParts, http::request::Parts};
 use axum_extra::{headers::{authorization::Bearer, Authorization}, TypedHeader};
+use tracing::info;
 use uuid::Uuid;
 
 use crate::app::{domain::{error::AppError, jwt_token::jwt_token::JwtAuthToken}, infrastructure::jwt_token::jwt_token::JwtAuthTokenImpl};
@@ -15,13 +18,17 @@ where
     type Rejection = AppError;
 
     async fn from_request_parts(req: &mut Parts, state: &S,) -> Result<Self, Self::Rejection> {
-        let TypedHeader(Authorization(bearer)) = TypedHeader::<Authorization<Bearer>>::from_request_parts(req, state).await
+        let TypedHeader(Authorization(bearer)) = TypedHeader::<Authorization<Bearer>>::from_request_parts(req, state)
+            .await
             .map_err(|_| AppError::Unauthorized(String::from("Authentication is required to access this resource")))?;
 
-        let Extension(token_service): Extension<JwtAuthTokenImpl> = Extension::from_request_parts(req, state).await
-                .map_err(|_| AppError::InternalServerError)?;
+        info!("attempt of getting JwtAuthTokenImpl from Extension");
+        let token_service = req.extensions.get::<Arc<JwtAuthTokenImpl>>()
+            .ok_or(AppError::Unauthorized(String::from("Authentication is required to access this resource")))?;
 
         let user_id = token_service.get_user_id_from_token(bearer.token())?;
+        info!("user id has been found");
+
         Ok(RequiredAuthentication(user_id))
 
         
