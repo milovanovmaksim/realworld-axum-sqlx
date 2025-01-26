8aa2c9384cffb02f2b619a330e0f7e153a1c1132
milovanovmaksim <milovanov160386@gmail.com>
2025-01-26 19:40:32 +0300

    deleted method from trait

--- src/app/domain/profile/repository/mod.rs
+++ src/app/domain/profile/repository/mod.rs
@@ -1,15 +1,6 @@
-use async_trait::async_trait;
-use entities::Profile;
-
-use crate::app::domain::{error::AppError, user::repository::entities::User};
-
 pub mod entities;
 
+use async_trait::async_trait;
+
 #[async_trait]
-pub trait ProfileRepository: Send + Sync + 'static {
-    async fn get_profile_by_name(
-        &self,
-        current_user: &User,
-        username: &str,
-    ) -> Result<Profile, AppError>;
-}
+pub trait ProfileRepository: Send + Sync + 'static {}
