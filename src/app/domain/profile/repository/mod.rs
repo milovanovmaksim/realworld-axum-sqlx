774d3109470adb832c81527fbde4da97c5e5dd59
milovanovmaksim <milovanov160386@gmail.com>
2025-01-26 23:05:12 +0300

    added docs for ProfileRepository

--- src/app/domain/profile/repository/mod.rs
+++ src/app/domain/profile/repository/mod.rs
@@ -1,6 +1,23 @@
 pub mod entities;
 
 use async_trait::async_trait;
+use uuid::Uuid;
 
+use crate::app::error::AppError;
+
+///
+/// ProfileRepository интерфейс, определяющий набор методов для работы с БД таблицы 'user_follows'.
 #[async_trait]
-pub trait ProfileRepository: Send + Sync + 'static {}
+pub trait ProfileRepository: Send + Sync + 'static {
+    ///
+    /// Возвращает true, если пользователь с following_user_id подписан
+    /// на пользователя с current_user_id.
+    /// current_user_id - текущий пользователь залогининый в системе;
+    /// following_user_id - пользователь, которого
+    /// хотим проверить подписан ли он на текущего пользователя.
+    async fn user_following(
+        &self,
+        current_user_id: Uuid,
+        following_user_id: Uuid,
+    ) -> Result<bool, AppError>;
+}
--- src/app/infrastructure/user/usecase.rs
+++ src/app/infrastructure/user/usecase.rs
@@ -1,6 +1,5 @@
-use std::sync::Arc;
-
 use async_trait::async_trait;
+use std::sync::Arc;
 use tracing::{error, info};
 
 use crate::app::{
