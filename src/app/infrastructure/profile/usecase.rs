633e10ab044326923cac3c304755d68d0cc2276d
milovanovmaksim <milovanov160386@gmail.com>
2025-01-26 20:17:03 +0300

    added ProfileUseCaseImpl struct

---
+++ src/app/infrastructure/profile/usecase.rs
@@ -0,0 +1,26 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+
+use crate::app::domain::{
+    self,
+    error::AppError,
+    profile::{repository::ProfileRepository, usecase::ProfileUseCase},
+    user::repository::{Email, UserRepository},
+};
+
+pub struct ProfileUseCaseImpl {
+    pub user_repository: Arc<dyn UserRepository>,
+    pub profile_repository: dyn ProfileRepository,
+}
+
+#[async_trait]
+impl ProfileUseCase for ProfileUseCaseImpl {
+    async fn get_profile(
+        &self,
+        current_user: Email,
+        username: String,
+    ) -> Result<domain::profile::usecase::responses::ProfileResponse, AppError> {
+        todo!()
+    }
+}
