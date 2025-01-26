0295864ccc66b0f365ae5fecc76aea290bb7d3c5
milovanovmaksim <milovanov160386@gmail.com>
2025-01-26 23:41:25 +0300

    added migration

---
+++ migrations/20250126203236_create_user_follows.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table if not exists user_follows
+(
+    id UUID primary key default uuid_generate_v4(),
+    created_at timestamptz not null default current_timestamp,
+    follower_id UUID not null references users (id) on delete cascade,
+    followee_id UUID not null references users (id) on delete cascade    
+);
