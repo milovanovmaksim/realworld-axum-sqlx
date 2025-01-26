79bf0c24bc9025c00ea8910b9d01c40936e67a20
milovanovmaksim <milovanov160386@gmail.com>
2025-01-26 23:16:00 +0300

    added doc string for ProfileUseCase

--- src/app/domain/profile/usecase/mod.rs
+++ src/app/domain/profile/usecase/mod.rs
@@ -4,8 +4,14 @@ use crate::app::{domain::user::repository::Email, error::AppError};
 use async_trait::async_trait;
 use responses::ProfileResponse;
 
+///
+/// ProfileUseCase интерфейс, определяющий бизнес логику профиля.
 #[async_trait]
 pub trait ProfileUseCase: Sync + Send + 'static {
+    ///
+    /// Возвращает профиль по username.
+    /// current_user - email текущего пользователя, прошедшего аутентификацию;
+    /// username - username запрашиваемого профиля.
     async fn get_profile(
         &self,
         current_user: Email,
