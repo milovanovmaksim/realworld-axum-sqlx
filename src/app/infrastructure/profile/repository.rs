5ed271c2579ab4904f961c799de40a01349bf9cd
milovanovmaksim <milovanov160386@gmail.com>
2025-01-27 21:29:48 +0300

    implemented user_following

---
+++ src/app/infrastructure/profile/repository.rs
@@ -0,0 +1,41 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use sqlx::query;
+use tracing::info;
+
+use crate::app::{
+    domain::profile::repository::ProfileRepository, error::AppError,
+    infrastructure::pgsql::db::PostgreSQL,
+};
+
+pub struct ProfileRepositoryImpl {
+    pg_sql: Arc<PostgreSQL>,
+}
+
+impl ProfileRepositoryImpl {
+    pub fn new(pg_sql: Arc<PostgreSQL>) -> Self {
+        Self { pg_sql }
+    }
+}
+
+#[async_trait]
+impl ProfileRepository for ProfileRepositoryImpl {
+    async fn user_following(
+        &self,
+        current_user_id: uuid::Uuid,
+        following_user_id: uuid::Uuid,
+    ) -> Result<bool, AppError> {
+        info!("Searching following user");
+        let record = query!(
+            r#"
+            select 1 as "id?" from user_follows where follower_id = $1 and followee_id = $2
+            "#,
+            current_user_id,
+            following_user_id
+        )
+        .fetch_optional(&self.pg_sql.pool())
+        .await?;
+        Ok(record.is_some())
+    }
+}
