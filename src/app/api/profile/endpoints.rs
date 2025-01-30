ce6504d37f1d2ca3a38f9f2e3882a25dabfe9c88
milovanovmaksim <milovanov160386@gmail.com>
2025-01-30 21:45:43 +0300

    added logger message

--- src/app/api/profile/endpoints.rs
+++ src/app/api/profile/endpoints.rs
@@ -1,18 +1,23 @@
 use std::{collections::HashMap, sync::Arc};
 
 use axum::{extract::Path, Extension, Json};
+use tracing::info;
 
 use crate::app::{
-    api::{self, extractors::optional_authentication::OptionalAuthentication, response::ApiResponse},
-    domain::profile::usecase::ProfileUseCase, infrastructure::profile::usecase::ProfileUseCaseImpl
+    api::{
+        self, extractors::optional_authentication::OptionalAuthentication, response::ApiResponse,
+    },
+    domain::profile::usecase::ProfileUseCase,
+    infrastructure::profile::usecase::ProfileUseCaseImpl,
 };
 
 pub async fn get_profile(
     Path(params): Path<HashMap<String, String>>,
     Extension(profile_usecase): Extension<Arc<ProfileUseCaseImpl>>,
-    OptionalAuthentication(user_id): OptionalAuthentication
+    OptionalAuthentication(user_id): OptionalAuthentication,
 ) -> ApiResponse<Json<api::profile::responses::ProfileResponse>> {
     let username = params.get("username").unwrap();
+    info!("recieved request to get profile {:?}", username);
     let profile = profile_usecase.get_profile(user_id, username).await?;
     todo!()
 }
