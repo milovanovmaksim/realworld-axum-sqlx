ea639de7dd65f5bba54831e6cd6dc6e5c773f758
milovanovmaksim <milovanov160386@gmail.com>
2025-01-30 21:35:52 +0300

    changed type username field

--- src/app/domain/user/repository/mod.rs
+++ src/app/domain/user/repository/mod.rs
@@ -16,6 +16,6 @@ pub trait UserRepository: Send + Sync + 'static {
     async fn login(&self, request: SigninUserRequest) -> Result<Option<User>, AppError>;
     async fn get_user_by_email(&self, email: Email) -> Result<Option<User>, AppError>;
     async fn get_user_by_id(&self, user_id: Uuid) -> Result<Option<User>, AppError>;
-    async fn get_user_by_username(&self, username: String) -> Result<Option<User>, AppError>;
+    async fn get_user_by_username(&self, username: &str) -> Result<Option<User>, AppError>;
     async fn update_user(&self, request: UpdateUserRequest) -> Result<User, AppError>;
 }
