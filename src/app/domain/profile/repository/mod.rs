be6829b17cad3d165cdd760e37b265e86bdb9474
milovanovmaksim <milovanov160386@gmail.com>
2025-02-01 22:56:35 +0300

    added remove_user_follow method in ProfileRepository trait

--- src/app/domain/profile/repository/mod.rs
+++ src/app/domain/profile/repository/mod.rs
@@ -11,19 +11,28 @@ use crate::app::error::AppError;
 #[async_trait]
 pub trait ProfileRepository: Send + Sync + 'static {
     ///
-    /// Возвращает true, если пользователь curretn_user_id является подписчиком пользователя followee_id.
-    /// current_user_id - id пользователя;
+    /// Возвращает true, если пользователь user_id является подписчиком пользователя followee_id.
+    /// user_id - id пользователя;
     /// followee_id - id пользователя на которого возможно подписан текущий пользователь.
-    async fn is_follower(&self, current_user_id: Uuid, followee_id: Uuid)
-        -> Result<bool, AppError>;
+    async fn is_follower(&self, user_id: Uuid, followee_id: Uuid) -> Result<bool, AppError>;
 
     ///
-    /// Создает новую запись в таблицу user_follow.
-    /// follower_id - id пользователя(подписчик);
+    /// Создает новую запись в таблицу user_follows.
+    /// follower_id - id пользователя;
     /// followee_id - id пользователя на которого подписываем follower_id.
     async fn add_user_follow(
         &self,
         follower_id: Uuid,
         followee_id: Uuid,
     ) -> Result<UserFollow, AppError>;
+
+    ///
+    /// Удаляет запись из таблицы user_follows.
+    /// follower_id - id подписчика;
+    /// followee_id - id пользователя от которого хотим отписаться.
+    async fn remove_user_follow(
+        &self,
+        follower_id: Uuid,
+        followee_id: Uuid,
+    ) -> Result<(), AppError>;
 }
