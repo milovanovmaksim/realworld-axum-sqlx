534f811d16fbf685e16fb5e022986e8ba92427f9
milovanovmaksim <milovanov160386@gmail.com>
2025-02-01 17:22:47 +0300

    wraped DiContainer into Arc

--- src/app/api/user/router.rs
+++ src/app/api/user/router.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::app::infrastructure::di::DiContainer;
 use axum::{
     routing::{get, post, put},
@@ -6,12 +8,12 @@ use axum::{
 
 use super::endpoints::{get_current_user, login, signup, update_user};
 
-pub fn user_router(di: DiContainer) -> Router {
+pub fn user_router(di: Arc<DiContainer>) -> Router {
     Router::new()
         .route("/users", post(signup))
         .route("/users/login", post(login))
         .route("/user", get(get_current_user))
         .route("/user", put(update_user))
-        .layer(Extension(di.user_usecase))
-        .layer(Extension(di.jwt_auth_token))
+        .layer(Extension(di.user_usecase.clone()))
+        .layer(Extension(di.jwt_auth_token.clone()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use app::{
     api::{config::ApiConfig, server::Server},
     infrastructure::di::DiContainer,
@@ -12,6 +14,6 @@ async fn main() {
         .expect("Failed to create di container");
     let api_config =
         ApiConfig::from_yaml("./config/config.yaml").expect("Failed to create api config");
-    let server = Server::new(di_container, api_config);
+    let server = Server::new(Arc::new(di_container), api_config);
     server.start().await.expect("Failed to run server");
 }
