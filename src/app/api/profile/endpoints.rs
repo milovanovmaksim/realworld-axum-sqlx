c1f12c8b0b578f27fbf4d53322a389926e5447c0
milovanovmaksim <milovanov160386@gmail.com>
2025-02-01 17:25:56 +0300

    added openapi doc for get_profile endpoint

--- src/app/api/profile/endpoints.rs
+++ src/app/api/profile/endpoints.rs
@@ -1,23 +1,39 @@
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 
-use axum::{extract::Path, Extension, Json};
+use axum::{extract, Extension, Json};
 use tracing::info;
 
 use crate::app::{
     api::{
-        self, extractors::optional_authentication::OptionalAuthentication, response::ApiResponse,
+        extractors::optional_authentication::OptionalAuthentication,
+        profile::responses::ProfileResponse, response::ApiResponse,
     },
     domain::profile::usecase::ProfileUseCase,
+    error::AppError,
     infrastructure::profile::usecase::ProfileUseCaseImpl,
 };
 
+#[utoipa::path(
+    get,
+    path = "/api/v1/profiles/{username}",
+    tag = "Profile",
+    description = "Get profile.", 
+    responses(
+            (status = StatusCode::OK, description = "Profile", body = ProfileResponse, content_type = "application/json"),
+            (status = StausCode::NOT_FOUND, description = "Profile not found.", body = HashMap<String, String>,
+                content_type = "application/json",
+                example = json!({"error": "User with username 'DonaldTrump' not found."})),
+            (status = StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = HashMap<String, String>,
+                content_type = "application/json",
+                example = json!({"error": AppError::InternalServerError.to_string()}))
+            ),
+)]
 pub async fn get_profile(
-    Path(params): Path<HashMap<String, String>>,
+    extract::Path(username): extract::Path<String>,
     Extension(profile_usecase): Extension<Arc<ProfileUseCaseImpl>>,
     OptionalAuthentication(user_id): OptionalAuthentication,
-) -> ApiResponse<Json<api::profile::responses::ProfileResponse>> {
-    let username = params.get("username").unwrap();
-    info!("recieved request to get profile {:?}", username);
+) -> ApiResponse<Json<ProfileResponse>> {
+    info!("Recieved request to get profile {:?}", username);
     let profile = profile_usecase.get_profile(user_id, username).await?;
-    todo!()
+    Ok(Json(ProfileResponse::from(profile)))
 }
