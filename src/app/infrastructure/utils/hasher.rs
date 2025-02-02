ea1ae06c9e364c60bc1a4019040069286f579b94
milovanovmaksim <milovanov160386@gmail.com>
2025-02-02 20:30:44 +0300

    added doc for hasher

--- src/app/infrastructure/utils/hasher.rs
+++ src/app/infrastructure/utils/hasher.rs
@@ -1,9 +1,14 @@
 use bcrypt::{hash, BcryptResult, DEFAULT_COST};
 
+
+///
+/// Генерирует хеш пароля.
 pub fn hash_password(naive_pw: &str) -> BcryptResult<String> {
     hash(naive_pw, DEFAULT_COST)
 }
 
+///
+/// Проверяет, что пароль эквивалентен предоставленному хешу.
 pub fn verify(naive_password: &str, hashed_password: &str) -> BcryptResult<bool> {
     bcrypt::verify(naive_password, hashed_password)
 }
