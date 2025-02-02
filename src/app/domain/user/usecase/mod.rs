db21c1c0150635a8879c70ed7b10c462b0a78acd
milovanovmaksim <milovanov160386@gmail.com>
2025-02-02 17:13:53 +0300

    added doc for UserUseCase

--- src/app/domain/user/usecase/mod.rs
+++ src/app/domain/user/usecase/mod.rs
@@ -8,11 +8,21 @@ use uuid::Uuid;
 
 use crate::app::error::AppError;
 
+///
+/// Интерфейс, определяющий набор методов бизнес логики пользователя.
 #[async_trait]
 pub trait UserUseCase: Send + Sync + 'static {
+    ///
+    /// Регистрирует нового пользователя.
     async fn signup(&self, request: SignupUserRequest) -> Result<UserUsecaseResponse, AppError>;
+
+    ///
+    /// Авторизация пользователя.
     async fn login(&self, request: SigninUserRequest) -> Result<UserUsecaseResponse, AppError>;
     async fn get_current_user(&self, user_id: Uuid) -> Result<UserUsecaseResponse, AppError>;
+
+    ///
+    /// Обновляет информацию о пользователе.
     async fn update_user(
         &self,
         request: (Uuid, UpdateUserRequest),
