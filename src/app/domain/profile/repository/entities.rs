2a35c8489fafe4ecb819f8464d7b3d5281549d9d
milovanovmaksim <milovanov160386@gmail.com>
2025-02-09 22:40:38 +0300

    delted Profile

--- src/app/domain/profile/repository/entities.rs
+++ src/app/domain/profile/repository/entities.rs
@@ -2,16 +2,6 @@ use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
 ///
-/// Содержит информацию о профиле.
-#[derive(Debug)]
-pub struct Profile {
-    pub username: String,
-    pub bio: Option<String>,
-    pub image: Option<String>,
-    pub following: bool,
-}
-
-///
 /// Представляет отдельную сущность в БД.
 #[derive(Debug)]
 pub struct UserFollow {
