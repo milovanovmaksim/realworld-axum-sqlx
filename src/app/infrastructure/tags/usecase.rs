5a3c4a1cfbdfb549ada423690c6c5ae645694f70
milovanovmaksim <milovanov160386@gmail.com>
2025-02-09 15:48:45 +0300

    implemnted get_tags for TagsUsecaseImpl

--- src/app/infrastructure/tags/usecase.rs
+++ src/app/infrastructure/tags/usecase.rs
@@ -1,25 +1,28 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
 
 use crate::app::domain::tags::{repository::TagsRepository, usecase::TagsUseCase};
 
 use super::repository::TagsRepositoryImpl;
 
+///
+/// Реализует набор методов уровня бизнес-логики тэга.
 pub struct TagsUsacaseImpl {
-    tags_repository: TagsRepositoryImpl,
+    tags_repository: Arc<TagsRepositoryImpl>,
 }
 
 impl TagsUsacaseImpl {
-    pub fn new(tags_repository: TagsRepositoryImpl) -> Self {
+    pub fn new(tags_repository: Arc<TagsRepositoryImpl>) -> Self {
         Self { tags_repository }
     }
 }
 
 #[async_trait]
 impl TagsUseCase for TagsUsacaseImpl {
-    async fn get_tags(
-        &self,
-        tags: Vec<String>,
-    ) -> Result<Vec<String>, crate::app::error::AppError> {
+    ///
+    /// Возвращает список тэгов по имени.
+    async fn get_tags(&self) -> Result<Vec<String>, crate::app::error::AppError> {
         Ok(self
             .tags_repository
             .get_tags(vec![])
