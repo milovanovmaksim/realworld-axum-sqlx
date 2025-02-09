67acfb5b7c12f1e69ae407095eea8a6502432dc7
milovanovmaksim <milovanov160386@gmail.com>
2025-02-09 20:02:08 +0300

    added Article

---
+++ src/app/domain/articles/repository/responses.rs
@@ -0,0 +1,13 @@
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+pub struct Article {
+    pub id: Uuid,
+    pub title: String,
+    pub body: String,
+    pub description: String,
+    pub slug: String,
+    pub user_id: Uuid,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
