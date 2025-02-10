05145a501676270e242fab22876ba4a2b9ba5559
milovanovmaksim <milovanov160386@gmail.com>
2025-02-10 21:36:16 +0300

    renamed Tag to TagEntity

--- src/app/domain/tags/repository/entities.rs
+++ src/app/domain/tags/repository/entities.rs
@@ -5,14 +5,14 @@ use uuid::Uuid;
 /// Тэг статьи. Содержит информацию о тэге.
 /// Представляет отдельную запись в БД.
 #[derive(Debug)]
-pub struct Tag {
+pub struct TagEntity {
     pub id: Uuid,
     pub tag: String,
     pub created_at: DateTime<Utc>,
 }
 
-impl From<Tag> for String {
-    fn from(entity: Tag) -> Self {
+impl From<TagEntity> for String {
+    fn from(entity: TagEntity) -> Self {
         entity.tag
     }
 }
--- src/app/domain/tags/repository/mod.rs
+++ src/app/domain/tags/repository/mod.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use entities::Tag;
+use entities::TagEntity;
 
 use crate::app::error::AppError;
 
@@ -12,9 +12,9 @@ pub trait TagsRepository {
     ///
     /// Возвращает тэги по имени.
     /// tags - вектор, сордержащий имена тэгов.
-    async fn get_tags(&self, tags: Vec<String>) -> Result<Vec<Tag>, AppError>;
+    async fn get_tags(&self, tags: Vec<String>) -> Result<Vec<TagEntity>, AppError>;
 
     ///
     /// Добавляет тэги в БД.
-    async fn create_tags(&self, tags: Vec<String>) -> Result<Vec<Tag>, AppError>;
+    async fn create_tags(&self, tags: Vec<String>) -> Result<Vec<TagEntity>, AppError>;
 }
