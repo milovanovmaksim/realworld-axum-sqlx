3ea67a8015dcba862e091e9c1fe4fe559b5d9eab
milovanovmaksim <milovanov160386@gmail.com>
2025-02-10 22:22:01 +0300

    added ArticleResponse

--- src/app/domain/articles/usecase/responses.rs
+++ src/app/domain/articles/usecase/responses.rs
@@ -1,5 +1,23 @@
-
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
 
 pub struct ArticleResponse {
-    
+    pub id: Uuid,
+    pub slug: String,
+    pub title: String,
+    pub description: String,
+    pub body: String,
+    pub tag_list: Vec<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub favorited: bool,
+    pub favorites_count: i64,
+    pub author: Author,
+}
+
+pub struct Author {
+    pub username: String,
+    pub bio: Option<String>,
+    pub image: Option<String>,
+    pub following: bool,
 }
